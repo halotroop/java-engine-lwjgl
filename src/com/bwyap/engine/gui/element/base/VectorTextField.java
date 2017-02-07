fe5d1b99d8ea821d52867a7db8a73a2c8d299a73
Benjamin Yap <bwyap@outlook.com>
2017-02-08 01:26:24 +1100

    Fix bug where text was scaled wrongly in TextField

--- src/com/bwyap/engine/gui/element/base/VectorTextField.java
+++ src/com/bwyap/engine/gui/element/base/VectorTextField.java
@@ -1,10 +1,7 @@
 package com.bwyap.engine.gui.element.base;
 
-import org.joml.Vector2f;
-
 import com.bwyap.engine.gui.element.properties.VectorShapeColourProperties;
 import com.bwyap.engine.gui.interfaces.IColouredVectorShape;
-import com.bwyap.engine.window.BoundsInterface;
 
 /**
  * A text field with a vector drawn background.
@@ -19,16 +16,6 @@ public abstract class VectorTextField extends TextField implements IColouredVect
 	public VectorTextField(int x, int y, float width, float height) {
 		super(x, y, width, height);
 		this.colours = new VectorShapeColourProperties();
-	}
-	
-	
-	@Override
-	public void updateBounds(BoundsInterface window, Vector2f parent) {
-		super.updateBounds(window, parent);
-		
-		if (!scaleAbsolute) {
-			text.setTextSize(text.getAbsoluteTextSize ()* ((scaleX + scaleY) / 2));
-		}
 	}
 	
 	
